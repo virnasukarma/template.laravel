a3203f641ab85a974496f4dfc5af3aab6e651fc6
virnasukarma <virnasukarma@gmail.com>
2020-08-06 10:58:11 +0700

    membuat template menggunakan blade laravel

---
+++ resources/views/adminlte/items/create.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte.master')
+
+@section('content')
+	<h2>Ini halaman create
+	</h2>
+
+@endsection
+
+@push('scripts')
+<script>
+  var wrappers = document.getElementsByClassName("wrappers");
+    var items = ["ini", "contoh"];
+
+    console.log(items)
+</script>
+@endpush
